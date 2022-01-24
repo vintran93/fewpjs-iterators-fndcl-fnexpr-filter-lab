29a1974e4d9be47927cf56ac1652d8023f4723c9
Vincent Tran <vincentuong@yahoo.com>
2022-01-24 17:57:27 -0600

    commit tree 3a8eb08324c274e5251b51b1001a74848c44bfb5

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === str.toLowerCase()
+    )
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(str.toLowerCase()) === 0
+    )
+}
+
+function matchName(drivers, stringName) {
+    return drivers.filter( record => record.name === stringName)
+}
